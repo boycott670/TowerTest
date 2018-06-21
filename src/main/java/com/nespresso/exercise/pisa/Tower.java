f8da33445a896b9ce130ed402f55d19d44ec0f9f
takingsss12 <takingsss12@gmail.com>
2018-06-21 10:27:59 +0100

    base

---
+++ src/main/java/com/nespresso/exercise/pisa/Tower.java
@@ -0,0 +1,17 @@
+package com.nespresso.exercise.pisa;
+
+public class Tower {
+
+    public void addFloor(int floorSize) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public String printFloor(int floorIndex) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public void addFloorWithWindows(int floorSize, int desiredNumberOfWindows) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+}
