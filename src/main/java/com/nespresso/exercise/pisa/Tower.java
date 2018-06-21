b9aba7b1f6308d49da356cde2738e6007bd51cc9
takingsss12 <takingsss12@gmail.com>
2018-06-21 10:38:10 +0100

    2/8

--- src/main/java/com/nespresso/exercise/pisa/Tower.java
+++ src/main/java/com/nespresso/exercise/pisa/Tower.java
@@ -1,17 +1,40 @@
 package com.nespresso.exercise.pisa;
 
-public class Tower {
+import java.util.ArrayList;
+import java.util.List;
 
-    public void addFloor(int floorSize) {
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
+public class Tower
+{
+  
+  private final List<Floor> floors;
+  
+  public Tower()
+  {
+    floors = new ArrayList<>();
+  }
 
-    public String printFloor(int floorIndex) {
-        throw new UnsupportedOperationException("Not yet implemented");
+  public void addFloor(int floorSize)
+  {
+    final Floor higherFloor = new Floor(floorSize);
+    
+    if (floors.isEmpty() || floors.get(floors.size() - 1).isValidAsHigherFloor(higherFloor))
+    {
+      floors.add(higherFloor);
     }
+    else
+    {
+      throw new IllegalArgumentException();
+    }
+  }
 
-    public void addFloorWithWindows(int floorSize, int desiredNumberOfWindows) {
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
+  public String printFloor(int floorIndex)
+  {
+    return floors.get(floorIndex).print();
+  }
+
+  public void addFloorWithWindows(int floorSize, int desiredNumberOfWindows)
+  {
+    throw new UnsupportedOperationException("Not yet implemented");
+  }
 
 }
